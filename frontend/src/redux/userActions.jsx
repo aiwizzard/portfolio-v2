93929326e9f77f695680111089e6fa5fbce1945c
aiwizzard <ajmalekvsroad@gmail.com>
2022-08-09 00:08:18 +0530

    set project details

--- frontend/src/redux/userActions.jsx
+++ frontend/src/redux/userActions.jsx
@@ -1,6 +1,11 @@
 import { UserActionTypes } from "./userActionTypes";
 
 export const setUserDetails = (user) => ({
-    type: UserActionTypes.SET_USER_DETAILS,
-    payload: user
-})
+  type: UserActionTypes.SET_USER_DETAILS,
+  payload: user,
+});
+
+export const setProjectDetails = (project) => ({
+  type: UserActionTypes.SET_PROJECT_DETAILS,
+  payload: project,
+});
--- frontend/src/redux/userReducer.jsx
+++ frontend/src/redux/userReducer.jsx
@@ -1,17 +1,19 @@
 import { UserActionTypes } from "./userActionTypes";
 
 const INITIAL_STATE = {
-    details: null,
+  details: null,
+  projectDetails: null,
 };
 
-const userReducer = (state=INITIAL_STATE, action) => {
-    switch (action.type) {
-        case UserActionTypes.SET_USER_DETAILS:
-            return {...state, details: action.payload}
-    
-        default:
-            return state;
-    }
-}
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UserActionTypes.SET_USER_DETAILS:
+      return { ...state, details: action.payload };
+    case UserActionTypes.SET_PROJECT_DETAILS:
+      return { ...state, projectDetails: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
