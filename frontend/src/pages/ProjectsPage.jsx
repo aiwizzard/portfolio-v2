89e3c20f5d70aa6da88ff1509d78c7afeacb25f5
aiwizzard <ajmalekvsroad@gmail.com>
2023-02-07 21:31:54 +0530

    get userdetails in projects page

--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -1,9 +1,32 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
+import { setUserDetails } from "../redux/userActions";
 import Projects from "../components/Projects";
 import TopNavBar from "../components/TopNavbar";
 
 function ProjectsPage() {
   const userDetails = useSelector((state) => state.user.details);
+
+  const dispatch = useDispatch();
+  const fetchData = () => {
+    axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}/api/v1/core/home/`,
+    })
+      .then((res) => {
+        dispatch(setUserDetails(res.data.data));
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    if (!userDetails) {
+      fetchData();
+    }
+  }, []);
   return (
     <>
       {userDetails && (
